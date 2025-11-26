579e1460b694b54e162c47971793c2d57dd09e8d
MathieuFortier2q <mathieupichu@gmail.com>
2025-11-26 13:38:36 -0500

    debut transformation EQ2

--- src/Jalon 3/EQ2 (Nous)/Transformation.sql
+++ src/Jalon 3/EQ2 (Nous)/Transformation.sql
@@ -1,47 +1,194 @@
+-- =============================================================================
+-- Dimensions
+-- =============================================================================
+
 -- Dim_Site
-SELECT nom AS nom_site,
+COPY (
+SELECT DISTINCT
+       nom AS nom_site,
        code AS code_site
-FROM staging_site;
+FROM staging_site
+) TO 'Dim_Site.csv' WITH (FORMAT CSV, HEADER);
 
 -- Dim_Zone
-SELECT code AS code_zone,
+COPY (
+SELECT DISTINCT
+       code AS code_zone,
        code_site AS code_site,
        nom AS nom_zone,
        description AS description
-FROM staging_zone;
-
--- Dim_Arbre
-
--- Dim_Parcelle
+FROM staging_zone
+) TO 'Dim_Zone.csv' WITH (FORMAT CSV, HEADER);
 
 -- Dim_Placette
+COPY (
+SELECT DISTINCT
+       plac AS placette_id,
+       zone AS code_zone,
+       date_eco AS date_creation
+FROM staging_placette
+) TO 'Dim_Placette.csv' WITH (FORMAT CSV, HEADER);
+
+-- Dim_Parcelle
+COPY (
+SELECT DISTINCT
+       pa.parcelle_id AS parcelle_id,
+       pa.placette_id AS placette_id,
+       pe.description AS peuplement,
+       pa.position AS position
+FROM staging_parcelle pa
+JOIN staging_peuplement pe
+ON pa.peuplement = pe.peuplement
+) TO 'Dim_Parcelle.csv' WITH (FORMAT CSV, HEADER);
 
 -- Dim_Plant
+COPY (
+SELECT DISTINCT
+       id AS plant_id,
+       date_eco AS date_decouverte
+FROM staging_plant
+) TO 'Dim_Plant.csv' WITH (FORMAT CSV, HEADER);
 
--- Dim_Site
+-- Dim_Arbre
+COPY (
+SELECT DISTINCT
+       arbre AS nom_arbre,
+       description AS description
+FROM staging_arbre
+) TO 'Dim_Arbre.csv' WITH (FORMAT CSV, HEADER);
 
--- Dim_Zone
-
--- Fact_Arbre
-
--- Fact_Couverture
-
--- Fact_Dimension
-
--- Fact_Etat
-
--- Fact_Floraison
-
--- Fact_Humidite
-
--- Fact_Note
-
--- Fact_Obstruction
-
--- Fact_Precipitation
-
--- Fact_Pression
+-- =============================================================================
+-- Facts: Processus de l'évolution de la météo
+-- =============================================================================
 
 -- Fact_Temperature
+COPY (
+SELECT z.id_interne AS id_interne_zone,
+       c.date_eco AS date,
+       c.temp_min AS temp_min,
+       c.temp_max AS temp_max,
+       CAST((c.temp_min + c.temp_max) / 2.0 AS DECIMAL(5,2)) AS temp_moyenne,
+       CAST((c.temp_max - c.temp_min) AS DECIMAL(5,2)) AS variation,
+       c.note AS note
+FROM staging_carnetmeteo c
+JOIN Dim_Zone z ON c.zone = z.code_zone
+) TO 'Fact_Temperature.csv' WITH (FORMAT CSV, HEADER);
+--manquant: id_interne_zone
+
+-- Fact_Humidite
+COPY (
+SELECT c.date_eco AS date,
+       c.hum_min AS hum_min,
+       c.hum_max AS hum_max,
+       CAST((c.hum_min + c.hum_max) / 2.0 AS DECIMAL(5,2)) AS hum_moyenne,
+       CAST((c.hum_max - c.hum_min) AS DECIMAL(5,2)) AS variation,
+       c.note AS note
+FROM staging_carnetmeteo c
+) TO 'Fact_Humidite.csv' WITH (FORMAT CSV, HEADER);
+--manquant: id_interne_zone
 
 -- Fact_Vents
+COPY (
+SELECT date_eco AS date,
+       vent_min AS vent_min,
+       vent_max AS vent_max,
+       CAST((c.vent_min + c.vent_max) / 2.0 AS DECIMAL(5,2)) AS vent_moyenne,
+       CAST((c.vent_max - c.vent_min) AS DECIMAL(5,2)) AS variation,
+       note AS note
+from staging_carnetmeteo c
+) TO 'Fact_Vents.csv' WITH (FORMAT CSV, HEADER);
+--manquant: id_interne_zone
+
+-- Fact_Pression
+COPY (
+SELECT date_eco AS date,
+       pres_min AS pres_min,
+       pres_max AS pres_max,
+       CAST((c.pres_min + c.pres_max) / 2.0 AS DECIMAL(5,2)) AS pres_moyenne,
+       CAST((c.pres_max - c.pres_min) AS DECIMAL(5,2)) AS variation,
+       note AS note
+FROM staging_carnetmeteo c
+) TO 'Fact_Pression.csv' WITH (FORMAT CSV, HEADER);
+--manquant: id_interne_zone
+
+-- Fact_Precipitation
+COPY (
+SELECT cm.date_eco AS date,
+       tp.libelle AS prec_nature,
+       cm.prec_tot AS prec_tot
+FROM staging_carnetmeteo cm
+JOIN staging_typeprecipitations tp
+ON cm.prec_nat = tp.code
+) TO 'Fact_Precipitation.csv' WITH (FORMAT CSV, HEADER);
+--manquant: id_interne_zone
+
+-- =============================================================================
+-- Facts: Processus de croissance d'une plante
+-- =============================================================================
+
+-- Fact_Dimension
+COPY (
+SELECT date_eco AS date,
+       longueur AS longueur,
+       largeur AS largeur,
+       CAST(d.longueur * d.largeur AS DECIMAL(10,2)) AS superficie,
+       note AS note
+FROM staging_obsdimension d
+) TO 'Fact_Dimension.csv' WITH (FORMAT CSV, HEADER);
+-- manquant: id_interne_plant, id_interne_parcelle
+
+-- Fact_Etat
+COPY (
+SELECT ob.date_eco AS date,
+       et.description AS etat,
+       ob.note AS note
+FROM staging_obsetat ob
+JOIN staging_etat et
+ON ob.etat = et.etat
+) TO 'Fact_Etat.csv' WITH (FORMAT CSV, HEADER);
+-- manquant: id_interne_plant, id_interne_parcelle
+
+-- Fact_Floraison
+COPY (
+SELECT date_eco AS date,
+       note AS note
+FROM staging_obsfloraison
+) TO 'Fact_Floraison.csv' WITH (FORMAT CSV, HEADER);
+-- manquant: id_interne_plant, id_interne_parcelle
+
+-- Fact_Note
+COPY (
+SELECT date_eco AS date,
+       note AS note
+FROM staging_plant_note
+) TO 'Fact_Note.csv' WITH (FORMAT CSV, HEADER);
+--manquant: id_interne_plant, id_interne_parcelle
+
+-- =============================================================================
+-- Facts: Environnement d'une placette
+-- =============================================================================
+
+-- Fact_Couverture
+COPY (
+SELECT type_couverture AS type_couverture,
+       taux AS taux
+FROM staging_placette_couverture
+) TO 'Fact_Couverture.csv' WITH (FORMAT CSV, HEADER);
+-- manquant: id_interne_placette, date
+
+-- Fact_Obstruction
+COPY (
+SELECT type_obs AS type_obstruction,
+       hauteur AS hauteur,
+       taux AS taux
+FROM staging_placette_obstruction
+) TO 'Fact_Obstruction.csv' WITH (FORMAT CSV, HEADER);
+--manquant: id_interne_placette, date
+
+-- Fact_Arbre
+COPY (
+SELECT rang AS rang
+FROM staging_placette_arbre
+) TO 'Fact_Arbre.csv' WITH (FORMAT CSV, HEADER);
+-- manquant: id_interne_placette, id_interne_arbre, date
+
