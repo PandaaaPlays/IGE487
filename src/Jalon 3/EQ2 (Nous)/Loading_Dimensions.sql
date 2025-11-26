f44bfcf7df31fe1cf8a746ee2d974fa8ee92fcee
MathieuFortier2q <mathieupichu@gmail.com>
2025-11-26 14:33:17 -0500

    fix fait plants et cles internes

--- src/Jalon 3/EQ2 (Nous)/Loading_Dimensions.sql
+++ src/Jalon 3/EQ2 (Nous)/Loading_Dimensions.sql
@@ -6,6 +6,6 @@ COPY Dim_Placette(placette_id, code_zone, date_creation) FROM '/EQ2/Loading/Dim_
 
 COPY Dim_Parcelle(parcelle_id, placette_id, peuplement, position) FROM '/EQ2/Loading/Dim_Parcelle.csv' WITH (FORMAT CSV, HEADER);
 
-COPY Dim_Plant(plant_id, date_decouverte) FROM '/EQ2/Loading/Dim_Plant.csv' WITH (FORMAT CSV, HEADER);
+COPY Dim_Plant(plant_id, parcelle_id, date_decouverte) FROM '/EQ2/Loading/Dim_Plant.csv' WITH (FORMAT CSV, HEADER);
 
 COPY Dim_Arbre(nom_arbre, description) FROM '/EQ2/Loading/Dim_Arbre.csv' WITH (FORMAT CSV, HEADER);
